8058cafe8d94a0a1de398a1312a1e4bb41cb8e01
Driver Station <fahrenheitrobotics@gmail.com>
2019-01-12 14:16:06 -0500

    Defined Transmission private variables. Created constructor.

--- src/main/java/frc/interfaces/Transmission.java
+++ src/main/java/frc/interfaces/Transmission.java
@@ -2,9 +2,24 @@ package frc.interfaces;
 
 // Import statements here
 import frc.globals.*;
-
+import edu.wpi.first.wpilibj.Victor;
 
 public class Transmission
 {
+    //Transmission varables
+    private Victor rightFront;
+    private Victor leftFront;
+    private Victor rightRear;
+    private Victor leftRear;
 
-} //end Transmission
+    public Transmission (Victor rf, Victor lf, Victor rr, Victor lr)
+    {
+        // Initialize motor controllers
+        rightFront = rf;
+        leftFront = lf;
+        rightRear = rr;
+        leftRear = lr;
+
+        // TODO: Invert one side of motor controllers so that + input moves forward
+    }
+} //end Transmission 
