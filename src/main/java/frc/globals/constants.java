bb3529ffce09c79578bb43e900ca7915b0ead171
Alexander M <Alexander.J.Malcolm18@gmail.com>
2019-01-12 13:10:50 -0500

    Defined controller indexes for USB, axis, and btns

--- src/main/java/frc/globals/constants.java
+++ src/main/java/frc/globals/constants.java
@@ -10,11 +10,29 @@ public class constants
     // ----------------------------------------
     // Controller constants
     // ----------------------------------------
+    // controller assignments
+    int driverController = 0;
+    int operaterController = 1;
+    
     // Axis Indexes
-
-
+    int leftX = 0;
+    int leftY = 1;
+    int leftTrigger = 2;
+    int rightTrigger = 3;
+    int rightX = 4;
+    int rightY = 5;
+    
     // Button Indexes
-
+    int btnA = 0;
+    int btnB = 1;
+    int btnX = 2;
+    int btnY = 3;
+    int btnLB = 4;
+    int btnRB = 5;
+    int btnBack = 6;
+    int btnStart = 7;
+    int btnLStick = 8;
+    int btnRStick = 9;
 
     // ----------------------------------------
     // Hardware Tunables
