905deada89a40057e9021e4fcbc961aeae38510b
Josh Luvera <jcluvera@yahoo.com>
2019-01-15 20:22:29 -0500

    Defined 2 new variables in DriveControls for right and left speed and added if statement the DriveControls class.

--- src/main/java/frc/interfaces/DriveControls.java
+++ src/main/java/frc/interfaces/DriveControls.java
@@ -9,10 +9,15 @@ public class DriveControls
 {
     //variables go here
     private static Joystick ctrl;
+    static double rightSpeeed;
+    static double leftSpeed;
 
     public DriveControls(Joystick dc)
     {
        ctrl = dc;
+       if (Math.abs(dc.getRawAxis(constants.rightX))> constants.gamepadDeadzone){
+
+       }
     }
    
 
