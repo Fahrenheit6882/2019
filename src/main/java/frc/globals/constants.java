ef5bb9d617b57ecd5889a126aa1a9835775fbab8
Jason <jasonlamey@gmail.com>
2019-01-15 19:31:41 -0500

    Formatted for readability

--- src/main/java/frc/globals/constants.java
+++ src/main/java/frc/globals/constants.java
@@ -11,26 +11,26 @@ public class constants
     // Controller constants
     // ----------------------------------------
     // controller assignments
-    public static int driverController = 0;
+    public static int driverController   = 0;
     public static int operaterController = 1;
     
     // Axis Indexes
-    public static int leftX = 0;
-    public static int leftY = 1;
-    public static int leftTrigger = 2;
+    public static int leftX        = 0;
+    public static int leftY        = 1;
+    public static int leftTrigger  = 2;
     public static int rightTrigger = 3;
-    public static int rightX = 4;
-    public static int rightY = 5;
+    public static int rightX       = 4;
+    public static int rightY       = 5;
     
     // Button Indexes
-    public static int btnA = 0;
-    public static int btnB = 1;
-    public static int btnX = 2;
-    public static int btnY = 3;
-    public static int btnLB = 4;
-    public static int btnRB = 5;
-    public static int btnBack = 6;
-    public static int btnStart = 7;
+    public static int btnA      = 0;
+    public static int btnB      = 1;
+    public static int btnX      = 2;
+    public static int btnY      = 3;
+    public static int btnLB     = 4;
+    public static int btnRB     = 5;
+    public static int btnBack   = 6;
+    public static int btnStart  = 7;
     public static int btnLStick = 8;
     public static int btnRStick = 9;
 
@@ -38,8 +38,8 @@ public class constants
     // Hardware Tunables
     // ----------------------------------------
     // Speed Factors
-    public static double driveFast = 0.6;
-    public static double driveSlow = 0.4;
+    public static double driveFast     = 0.6;
+    public static double driveSlow     = 0.4;
     public static double eleveatorFast = 0.7;
     public static double eleveatorSlow = 0.3;
     
