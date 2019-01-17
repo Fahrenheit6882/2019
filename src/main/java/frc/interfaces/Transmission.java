cf36dde02c401b3c612ec48e9801f2f3791dd481
Driver Station <fahrenheitrobotics@gmail.com>
2019-01-16 20:20:51 -0500

    finished adjustment based on speed.

--- src/main/java/frc/interfaces/Transmission.java
+++ src/main/java/frc/interfaces/Transmission.java
@@ -11,7 +11,7 @@ public class Transmission
     private Victor leftFront;
     private Victor rightRear;
     private Victor leftRear;
-
+    private static boolean fast = true;
     public Transmission (Victor rf, Victor lf, Victor rr, Victor lr)
     {
         // Initialize motor controllers
@@ -42,7 +42,26 @@ public class Transmission
         {
             rightSpeed = 1.0;
         }
-        
-    }
+        //pressing button A turn fast false 
+        if(hardware.driverGamepad.getRawButtonPressed(constants.btnA))
+        {
+            fast = false;
+        }
+        //pressing button X turn fast true
+        if(hardware.driverGamepad.getRawButtonPressed(constants.btnX))
+        {
+            fast = true;
+        }
+        //checking if fast is true/false to adjust left & right speed
+        if(fast = true)
+        {
+            leftFront = (leftSpeed * constants.driveFast);
+            leftRear = (leftSpeed * constants.driveFast);  
+        }else
+            {
+                rightFront = (rightSpeed *constants.driveSlow);
+                rightRear = (rightSpeed * constants.driveSlow); 
+            } 
+    
     
 } //end Transmission
