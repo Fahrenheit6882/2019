4387467d19f2e6b73ad9dc57dd85d8946052e86a
Josh Luvera <jcluvera@yahoo.com>
2019-01-31 18:31:37 -0500

    Added logic statements for claw presets.

--- src/main/java/frc/interfaces/OperatorControls.java
+++ src/main/java/frc/interfaces/OperatorControls.java
@@ -41,14 +41,14 @@ public class OperatorControls
             clawSpeed = 0.0;
         }
         /*Elavator preset positions
-        * up dpad is medium RocketShip for HatchPanel
-        * down dpad is lower RocketShip/CargoShip/LoadingZone for HatchPanel
-        *
         * up dpad + left trigger is medium RocketShip for Cargo
         * down dpad + left trigger is lower RocketShip for Cargo
         *
         * up dpad + left bumper is high CargoShip for Cargo
         * down dpad +left bumper is pick up Cargo from floor
+        *
+        * up dpad is medium RocketShip for HatchPanel
+        * down dpad is lower RocketShip/CargoShip/LoadingZone for HatchPanel
         */
         if (hardware.operatorGamepad.getRawButtonPressed(constants.leftTrigger))
         {
@@ -85,8 +85,30 @@ public class OperatorControls
                 }
             }
         }
+        /*Manipulator/claw preset positions
+        *A is open for Cargo
+        *B is close for Hatch Panel
+        *X is close for Cargo
+        *Y is open for Hatch Panel
+        */
+        if(hardware.operatorGamepad.getRawButtonPressed(constants.btnA))
+        {
+            //Open for Cargoo
+        }
+        if(hardware.operatorGamepad.getRawButtonPressed(constants.btnB))
+        {
+            //Close for Hatch Panel
+        }
+        if(hardware.operatorGamepad.getRawButtonPressed(constants.btnX))
+        {
+            //Close for Cargo
+        }
+        if(hardware.operatorGamepad.getRawButtonPressed(constants.btnY))
+        {
+            //Open for Hatch Panel
+        }
     }
-        /**
+        /*
         * Method: operatorInput
         * Parameters: N/A
         * Return: void
