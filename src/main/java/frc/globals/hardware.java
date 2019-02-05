5dcffa94d2c5e94f7f1b7acf437ac16cf5b4fed8
Josh Luvera <jcluvera@yahoo.com>
2019-02-05 18:46:08 -0500

    Initialized Talons for Elevator.

--- src/main/java/frc/globals/hardware.java
+++ src/main/java/frc/globals/hardware.java
@@ -105,7 +105,7 @@ public class hardware
     // ***********************************************
     // INTERFACES
     // ***********************************************
-    public static Elevator lift = new Elevator();
+    public static Elevator lift = new Elevator(new TalonSRX(0), new TalonSRX(1));
     //Initializes Transmission with four Victor motor controllers: front right, back right, front left, back left.
     public static Transmission driveBase = new Transmission(new VictorSPX(0), new VictorSPX(1), new VictorSPX(2), new VictorSPX(3), new Encoder(1, 2, false, Encoder.EncodingType.k4X), new Encoder(3, 4, false, Encoder.EncodingType.k4X));
         
